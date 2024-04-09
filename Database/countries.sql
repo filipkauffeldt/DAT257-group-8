02eade3a88aae9be653e67b93422c0f0d3c6b3d3
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2024-04-09 20:54:45 +0200

    Create tabels for Countries, Data and DataPoints in sql

---
+++ Database/countries.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Countries (
+    code TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    continent TEXT,
+    description TEXT
+);
+
+CREATE TABLE Data (
+    id INT PRIMARY KEY,
+    name TEXT NOT NULL,
+    country TEXT NOT NULL,
+    description TEXT,
+    FOREIGN KEY (country) REFERENCES Countries (code),
+    UNIQUE (name, country)
+);
+
+CREATE TABLE DataPoints (
+    id INT PRIMARY KEY,
+    data_id INT NOT NULL,
+    key TEXT NOT NULL,
+    value FLOAT NOT NULL,
+    UNIQUE (data_id, key),
+    FOREIGN KEY (data_id) REFERENCES Data (id)
+);
