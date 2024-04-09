c2a7a7ec4bf0dc71a36f08a8a02b9c4bdea6e69b
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2024-04-09 13:28:22 +0200

    Make objects immutable

--- API/Contracts/DataPoint.cs
+++ API/Contracts/DataPoint.cs
@@ -2,7 +2,7 @@
 {
     public class DataPoint
     {
-        public required string Key { get; set; }
-        public required double Value { get; set; }
+        public required string Key { get; init; }
+        public required double Value { get; init; }
     }
 }
