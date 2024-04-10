d1b8db79f5e4a1b33f5e56ec3cd57be431083b56
Huffla <dator343@gmail.com>
2024-04-10 10:43:16 +0200

    Required

--- API/Contracts/Data.cs
+++ API/Contracts/Data.cs
@@ -6,7 +6,7 @@ namespace API.Contracts
     {
         public required string Name { get; init; }
         public string? Description { get; init; }
-        public string? Unit {  get; init; }
+        public required string Unit {  get; init; }
         public required Collection<DataPoint> Points { get; init; }
     }
 }
