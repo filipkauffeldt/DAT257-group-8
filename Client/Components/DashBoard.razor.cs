c476ef00f5e8a9f642069fb1901d45399800c2c8
Flaskhals68 <03kajo@gmail.com>
2024-04-11 13:04:31 +0200

    Make DashBoard make api calls

--- Client/Components/DashBoard.razor.cs
+++ Client/Components/DashBoard.razor.cs
@@ -1,7 +1,25 @@
-﻿namespace Client.Components
+﻿using System.Net.Http.Json;
+using API.Contracts;
+using Radzen.Blazor;
+
+
+namespace Client.Components
 {
     public partial class DashBoard
     {
-        
+        private Country? countryOfTheDay;
+        private List<Country> data = new List<Country>(); 
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                countryOfTheDay = await httpClient.GetFromJsonAsync<Country>("https://localhost:7262/Country/GetCountryOfTheDay");
+                Country baseCountry = await httpClient.GetFromJsonAsync<Country>("https://localhost:7262/Country/GetCountry?id=sweden");
+                data.Add(baseCountry);
+            }
+            catch (Exception e) { 
+                Console.WriteLine(e);
+            }
+        }
     }
 }
