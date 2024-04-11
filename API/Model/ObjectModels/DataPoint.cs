14d5e987ab019611fabddfd65ab997b4a6dd21fc
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2024-04-12 01:56:28 +0200

    Add object models for Country, Data, and DataPoint

---
+++ API/Model/ObjectModels/DataPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Model.ObjectModels
+{
+    [Table("datapoints")]
+    public class DataPoint
+    {
+        public long DP_Id { get; set; }
+        public required DateOnly Date { get; set; }
+        public required double Value { get; set; }
+    }
+}
