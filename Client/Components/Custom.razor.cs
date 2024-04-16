fefabe2f8c856e0f038a76ffa6146661b30338ac
Noah Andreasson <noahan@student.chalmers.se>
2024-04-16 19:32:33 +0200

    Add new page to navbar
    Add a new page to the navbar

---
+++ Client/Components/Custom.razor.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Json;
+using System.Reflection.Metadata.Ecma335;
+using API;
+using Client.API;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen.Blazor;
+
+
+namespace Client.Components
+{
+    public partial class Custom
+    {
+        private List<string> availableCountries = new List<string>() { "swe", "nor", "fin", "den" };
+
+        private Country homeCountry = new Country()
+        {
+            Name = "Sweden",
+            Code = "SE",
+            Data = [new Data() { Name = "Water", Unit = "L", Points = [new DataPoint() { Date = new DateOnly(2023, 1, 1), Value = 100 }] }]
+        };
+
+        private Country otherCountry = new Country()
+        {
+            Name = "Norway",
+            Code = "NO",
+            Data = [new Data() { Name = "Water", Unit = "L", Points = [new DataPoint() { Date = new DateOnly(2023, 1, 1), Value = 200 }] }]
+        };
+        DateOnly dateOnlyLol = new DateOnly(2023, 1, 1);
+        // Dummy labels
+        private readonly string[] dataLabels = { "Water", "Electricity", "CO2" };
+
+        protected override async Task OnInitializedAsync()
+        {
+            otherCountry = await apiHandler.FetchCountryOfTheDay(httpClient);
+            homeCountry = await apiHandler.FetchCountry("swe", httpClient);
+        }
+    }
+}
+    
