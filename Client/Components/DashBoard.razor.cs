1cf82247078e99bbdc3b315f1a6964eb030a38cf
Flaskhals68 <03kajo@gmail.com>
2024-04-16 14:07:57 +0200

    Display card for each data entry
    This will display a card for each data entry with a Name attribute that is shared by both countries.

--- Client/Components/DashBoard.razor.cs
+++ Client/Components/DashBoard.razor.cs
@@ -17,23 +17,46 @@ namespace Client.Components
         {
             Name = "Bulgaria",
             Code = "BU",
-            Data = [new Data() { Name = "Water", Unit = "L", Points = [new DataPoint() { Date = new DateOnly(2023,1,1), Value = 100}] }]
+            Data = [new Data() { Name = "Water", Unit = "L", Points = [new DataPoint() { Date = new DateOnly(2023,1,1), Value = 100}] },
+                    new Data() {Name = "CO2 Emissions", Unit = "Kg", Points = [new DataPoint() { Date = new DateOnly(2023, 1, 1), Value = 200 }]}]
         };
         private Country countryCompTwo = new Country()
         {
             Name = "BOgusland",
             Code = "BO",
-            Data = [new Data() { Name = "Water", Unit = "L", Points = [new DataPoint() { Date = new DateOnly(2023, 1, 1), Value = 200 }] }]
+            Data = [new Data() { Name = "Water", Unit = "L", Points = [new DataPoint() { Date = new DateOnly(2023, 1, 1), Value = 200 }] },
+                    new Data() {Name = "CO2 Emissions", Unit = "Kg", Points = [new DataPoint() { Date = new DateOnly(2023, 1, 1), Value = 200 }]}]
         };
-        DateOnly dateOnlyLol = new DateOnly(2023, 1, 1);
+        DateOnly date = new DateOnly(2023, 1, 1);
         // Dummy labels
         private readonly string[] dataLabels = { "Water", "Electricity", "CO2" };
+        private IEnumerable<string> dataMetrics = new List<string>();
 
         protected override async Task OnInitializedAsync()
         {
-            countryOfTheDay = await apiHandler.FetchCountryOfTheDay(httpClient);
-            var homeCountry = await apiHandler.FetchCountry("swe", httpClient);
-            comparedCountries.Add(homeCountry);
+            //countryOfTheDay = await apiHandler.FetchCountryOfTheDay(httpClient);
+            //var homeCountry = await apiHandler.FetchCountry("swe", httpClient);
+            //comparedCountries.Add(homeCountry);
+            dataMetrics = GetSharedMetrics();
+        }
+
+        private IEnumerable<string> GetSharedMetrics()
+        {
+            if (countryComp == null || countryComp.Data == null ||
+                countryCompTwo == null || countryCompTwo.Data == null) {
+                return Enumerable.Empty<string>();
+            }
+
+            var sharedMetrics = countryComp.Data
+                .Select(data => data.Name)
+                .Intersect(countryCompTwo.Data.Select(data => data.Name));
+
+            return sharedMetrics;
+        }
+
+        private string FormatLabel(string label)
+        {
+            return $"{label.Replace(" ", "-")}-statistic";
         }
     }
 }
