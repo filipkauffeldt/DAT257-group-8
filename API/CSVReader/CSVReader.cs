f82127c1d22e12d750c9a682123c4205f41b0c9b
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2024-04-17 22:27:46 +0200

    Read data from files and populate objects and put in database

--- API/CSVReader/CSVReader.cs
+++ API/CSVReader/CSVReader.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using API.Model.ObjectModels;
 
 namespace CSVReader
 {
     public class CSVReader
     {
-    
-
         public static List<Country> ReadCountries(string path)
         {
             List<Country> countries = new List<Country>();
@@ -20,7 +17,10 @@ namespace CSVReader
                     while (!reader.EndOfStream)
                     {
                         string line = reader.ReadLine();
-                        string[] values = line.Split(',');
+ 
+                        var values = Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+                        values[1] = values[1].Replace("\"", string.Empty);
 
                         var country = new Country()
                         {
@@ -36,6 +36,83 @@ namespace CSVReader
             catch (Exception ex)
             {
                 Console.WriteLine("Error reading CSV file: " + ex.Message);
+            }
+
+            return countries;
+        }
+        
+        public static List<Country> AddData(List<Country> countries, string path)
+        {
+            var dataMap = new Dictionary<string, Data>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        try
+                        {
+
+                            string line = reader.ReadLine();
+
+                            var values = Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+                            values[0] = values[0].Replace("\"", string.Empty);
+                            values[1] = values[1].Replace("\"", string.Empty);
+                            values[2] = values[2].Replace("\"", string.Empty);
+                            values[3] = values[3].Replace("\"", string.Empty);
+                            values[4] = values[4].Replace("\"", string.Empty);
+
+                            var dataName = values[0];
+                        
+                            var countryName = values[1];
+                            var unit = values[2];
+                            var year = values[3];
+                            var value = double.Parse(values[4], CultureInfo.InvariantCulture);
+
+                            Console.WriteLine($"Add data for {countryName}");
+
+                            if (!dataMap.ContainsKey(countryName))
+                            {
+                                var newData = new Data()
+                                { 
+                                    Name = dataName,
+                                    Unit = unit,
+                                    Points = new List<DataPoint>()
+                                };
+
+                                dataMap.Add(countryName, newData);
+                            }
+
+                            var dp = new DataPoint()
+                            {
+                                Date = new DateOnly(int.Parse(year), 1, 1),
+                                Value = value
+                            };
+
+                            var countryData = dataMap[countryName];
+                            var points = countryData.Points;
+                            points.Add(dp);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Error adding data: {e.Message}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error adding data: {ex.Message}");
+            }
+
+            foreach ((var n, var d) in dataMap)
+            {
+                Console.WriteLine($"Country: {n}, Data: {d.Name}");
+                countries.Where(c => c.Name == n)
+                    .ToList()
+                    .ForEach(c => c.Data.Add(d));
             }
 
             return countries;
--- API/CSVReader/Program.cs
+++ API/CSVReader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using API.Model;
 using API.Model.ObjectModels;
+using ZetaLongPaths;
 
 namespace CSVReader
 {
@@ -8,12 +9,30 @@ namespace CSVReader
         static void Main(string[] args)
         {
             var repository = new CountryRepository(new CountryDbContext());
-            var path = "";
-            List<Country> countries = CSVReader.ReadCountries(path);
+            var path = new ZlpDirectoryInfo(@""); // Add path to file with countries
+            var basePath = new ZlpDirectoryInfo(@""); // Add path to directory with files containing data
+
+            var countryFile = path.GetFiles(SearchOption.TopDirectoryOnly);
+            var countries = CSVReader.ReadCountries(countryFile.First().ToString());
+
+            var dataFiles = basePath.GetFiles(SearchOption.AllDirectories).ToList();
+
+            foreach (var df in dataFiles) 
+            {
+                Console.WriteLine($"Opening: {df.ToString()}");
+                try
+                { 
+                    countries = CSVReader.AddData(countries, df.ToString());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
 
             foreach (var country in countries)
             {
-                Console.WriteLine(country.Code + " " + country.Name + " " + country.Continent);
+                Console.WriteLine($"{country.Code} {country.Name} {country.Continent}");
 
                 repository.AddCountry(country);
             }
