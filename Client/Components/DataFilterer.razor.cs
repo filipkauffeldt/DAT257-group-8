c9f673933bf13a23b794f35b191f5b0f5fc01254
erbergli <112847802+erbergli@users.noreply.github.com>
2024-04-18 18:18:12 +0200

    remove mock data

--- Client/Components/DataFilterer.razor.cs
+++ Client/Components/DataFilterer.razor.cs
@@ -2,6 +2,8 @@
 using API;
 using System.Linq;
 using Radzen.Blazor;
+using Client.API;
+using System.Net.Http;
 
 namespace Client.Components
 {
@@ -9,42 +11,13 @@ namespace Client.Components
     {
 
         IEnumerable<Data> data;
-        private Country countryCompTwo = new Country()
-        {
-            Name = "Bogusland",
-            Code = "BO",
-            Data = [
-                new Data() { Name = "Water", Unit = "L", Points = [new DataPoint() { Date = new DateOnly(2023, 1, 1), Value = 200 }] },
-                new Data() { Name = "Oil", Unit = "L", Points = [new DataPoint() { Date = new DateOnly(2023, 1, 1), Value = 200 }] },
-                new Data() { Name = "Energy", Unit = "L", Points = [new DataPoint() { Date = new DateOnly(2023, 1, 1), Value = 200 }] }
-                ]
-        };
 
         protected override async Task OnInitializedAsync()
         {
-
             await base.OnInitializedAsync();
-
-            //Country exampleCountry = await apiHandler.FetchCountry("SWE", httpClient);
-
-            //var countryData = exampleCountry.Data;
-            var countryData = countryCompTwo.Data;
-            data = countryData;
-
-
+            var countryData = await apiHandler.FetchCountryOfTheDay(httpClient);
+            data = countryData.Data;
         }
-
-        private IList<string> PopulateData(Country country)
-        {
-            var countryData = countryCompTwo.Data;
-            var dataNames = new List<string>();
-            foreach (Data cd in countryData)
-            {
-                dataNames.Add(cd.Name);
-            }
-            return dataNames;
-        }
-
 
     }
 }
