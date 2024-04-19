bebe2358a5a5f801d5c1e6e07757d87d0e31db97
erbergli <erik.berglind@gmail.com>
2024-04-19 15:03:38 +0200

    Able to get actual data from database as values in dropdown

--- Client/Components/DataFilterer.razor.cs
+++ Client/Components/DataFilterer.razor.cs
@@ -5,24 +5,25 @@ using Radzen.Blazor;
 using Client.API;
 using System.Net.Http;
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 
 namespace Client.Components
 {
     public partial class DataFilterer
     {
+        [Parameter]
+        public required IEnumerable<string> Data { get; set; }
 
-        IEnumerable<Data> data = new Collection<Data>();
+        private IList<string> values;
+
+
+        //[Parameter]
+        //public required Country Country { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
-
-            var countryData = await apiHandler.FetchCountryOfTheDay(httpClient);
-            data = countryData.Data;
-
-
-            
+            values = Data.ToList();
         }
-
     }
 }
