ca66885773c60d65b1f7ec71a158dc374acd7e92
erbergli <erik.berglind@gmail.com>
2024-04-19 12:44:06 +0200

    Small changes

--- Client/Components/DataFilterer.razor.cs
+++ Client/Components/DataFilterer.razor.cs
@@ -4,19 +4,24 @@ using System.Linq;
 using Radzen.Blazor;
 using Client.API;
 using System.Net.Http;
+using System.Collections.ObjectModel;
 
 namespace Client.Components
 {
     public partial class DataFilterer
     {
 
-        IEnumerable<Data> data;
+        IEnumerable<Data> data = new Collection<Data>();
 
         protected override async Task OnInitializedAsync()
         {
             await base.OnInitializedAsync();
+
             var countryData = await apiHandler.FetchCountryOfTheDay(httpClient);
             data = countryData.Data;
+
+
+            
         }
 
     }
