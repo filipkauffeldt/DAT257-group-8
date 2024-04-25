0e4870c13e47e01a8b6ff7dbd4ba2326f210118c
Flaskhals68 <03kajo@gmail.com>
2024-04-25 12:45:50 +0200

    Fix ApiHandlerTest

--- TestClient/API/TestApiRequestHandler.cs
+++ TestClient/API/TestApiRequestHandler.cs
@@ -1,6 +1,8 @@
 using API;
 using Client.API;
 using System.Net.Http.Json;
+using Moq;
+using Moq.Protected;
 
 namespace TestClient.API
 {
@@ -8,13 +10,13 @@ namespace TestClient.API
     {
         private readonly ApiRequestHandler _apiHandler = new ApiRequestHandler(new GeoLocatorHandler());
         private readonly HttpClient _httpClient = new HttpClient();
-        private readonly string _url = "https://localhost:7262";
+        private readonly string _url = "http://localhost:5016";
 
         [Fact]
         public async Task TestFetchCountryOfTheDayCorrectType()
         {
             Country country = await _apiHandler.FetchCountryOfTheDay(_httpClient);
-            Assert.True(country != null && country.GetType() ==  typeof(Country));
+            Assert.True(country != null && country.GetType() == typeof(Country));
         }
 
         [Fact]
@@ -38,21 +40,40 @@ namespace TestClient.API
             string iso = "SWE";
             Country apiHandlerCountry = await _apiHandler.FetchCountry(iso, _httpClient);
             Country rawFetchCountry = await _httpClient.GetFromJsonAsync<Country>($"{_url}/Country/GetCountry/{iso}");
-            //Assert.Equal(apiHandlerCountry, rawFetchCountry);
             Assert.True(apiHandlerCountry.Equals(rawFetchCountry));
         }
 
         [Fact]
-        public async Task FetchCountryDataByYearCorrectEndpoint()
+        public async Task TestFetchCountryDataByYearCorrectEndpoint()
         {
-            var year = new DateOnly(2020, 1, 1);
-            string code = "SWE";
+            var code = "SWE";
+            var date = new DateOnly(2020, 1, 1);
+            var mockMsgHandler = new Mock<HttpMessageHandler>();
+            mockMsgHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(r => r.Method == HttpMethod.Get && r.RequestUri == new Uri($"{_url}/Country/GetCountryDataForYear/{code}/{date.Year}-{date.Month}-{date.Day}")),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = System.Net.HttpStatusCode.OK,
+                    Content = new StringContent(System.Text.Json.JsonSerializer.Serialize(new Country
+                    {
+                        Code = "CODE",
+                        Name = "Country Name"
+                    }))
+                });
 
-            var apiHandlerData = await _apiHandler.FetchCountryByYear(_httpClient, code, year);
-            var rawFetchData = await _httpClient.GetFromJsonAsync<Country>($"{_url}/Country/GetCountryDataForYear/{code}/{year}");
+            var mockClient = new HttpClient(mockMsgHandler.Object);
+            var mockResponse = await _apiHandler.FetchCountryByYear(mockClient, code, date);
 
-            var dataDiff = apiHandlerData.Data?.Intersect(rawFetchData.Data);
-            Assert.True(dataDiff != null && !dataDiff.Any());
+            mockMsgHandler.Protected().Verify(
+                "SendAsync",
+                Times.Once(),
+                ItExpr.Is<HttpRequestMessage>(req =>
+                    req.Method == HttpMethod.Get && req.RequestUri == new Uri($"{_url}/Country/GetCountryDataForYear/{code}/{date.Year}-{date.Month}-{date.Day}")),
+                ItExpr.IsAny<CancellationToken>());
+            Assert.Equal(new Country { Code = "CODE", Name = "Country Name" }, mockResponse);
         }
     }
 }
