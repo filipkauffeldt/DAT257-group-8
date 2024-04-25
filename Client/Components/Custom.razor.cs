bce7f17aa18822bfa697bf98bdff7080361424ea
BooloCaptain <112878577+BooloCaptain@users.noreply.github.com>
2024-04-25 10:58:43 +0200

    Link Comparison to Navbar and fix card update
    Link Comparison page to navbar so that you can switch to it. Also make
    statistics cards update when dropdowns are changed.

--- Client/Components/Custom.razor.cs
+++ Client/Components/Custom.razor.cs
@@ -19,36 +19,39 @@ namespace Client.Components
 
         private string selectedOther = "";
 
-        private Country countryComp { get; set; }
+        private Country _country { get; set; }
         
-        private Country countryCompTwo { get; set; }
+        private Country _countryToCompareWith { get; set; }
 
         private IList<string> availableCountries = new List<string>() {"SWE", "BGR", "NOR", "DNK", "FIN", "ISL"};
         
-        DateOnly date = new DateOnly(2022, 1, 1);
-        private IList<string> dataMetrics = new List<string>();
+        private DateOnly _date = new DateOnly(2022, 1, 1);
+        private IList<string> _dataMetrics = new List<string>();
+
+        private IList<string> _availableMetrics = new List<string>();
 
         protected override async Task OnInitializedAsync()
         {
-            countryComp = await apiHandler.FetchCountryByYear(httpClient, "SWE", date); // Sweden
-            countryCompTwo = await apiHandler.FetchCountryByYear(httpClient, "BGR", date); // Bulgaria
-            selectedHome = countryComp.Code;
-            selectedOther = countryCompTwo.Code;
-            dataMetrics = GetValidMetrics();
+            _country = await apiHandler.FetchCountryByYear(httpClient, "SWE", _date); // Sweden
+            _countryToCompareWith = await apiHandler.FetchCountryByYear(httpClient, "BGR", _date); // Bulgaria
+            selectedHome = _country.Code;
+            selectedOther = _countryToCompareWith.Code;
+            _dataMetrics = GetValidMetrics();
+            _availableMetrics = _dataMetrics;
         }
 
         private IList<string> GetValidMetrics()
         {
-            if (countryComp == null || countryComp.Data == null ||
-                countryCompTwo == null || countryCompTwo.Data == null) {
+            if (_country == null || _country.Data == null ||
+                _countryToCompareWith == null || _countryToCompareWith.Data == null) {
                 return new List<string>();
             }
 
             var validMetrics = new List<string>();
-            foreach (var metric in countryComp.Data.Select(d => d.Name).ToList())
+            foreach (var metric in _country.Data.Select(d => d.Name).ToList())
             {
-                var countryCompDataExists = countryComp.Data?.Any(d => d.Name == metric && d.Points.Any()) ?? false;
-                var countryCompTwoDataExists = countryCompTwo.Data?.Any(d => d.Name == metric && d.Points.Any()) ?? false;
+                var countryCompDataExists = _country.Data?.Any(d => d.Name == metric && d.Points.Any()) ?? false;
+                var countryCompTwoDataExists = _countryToCompareWith.Data?.Any(d => d.Name == metric && d.Points.Any()) ?? false;
 
                 if (countryCompDataExists && countryCompTwoDataExists)
                 {
@@ -62,22 +65,30 @@ namespace Client.Components
         private async void UpdateHomeCountry(string code)
         {
             selectedHome = code;
-            countryComp = await apiHandler.FetchCountryByYear(httpClient, code, date);
-            dataMetrics = GetValidMetrics();
+            _country = await apiHandler.FetchCountryByYear(httpClient, code, _date);
+            _dataMetrics = GetValidMetrics();
             StateHasChanged();
         }
 
         private async void UpdateOtherCountry(string code)
         {
             selectedOther = code;
-            countryCompTwo = await apiHandler.FetchCountryByYear(httpClient, code, date);
-            dataMetrics = GetValidMetrics();
+            _countryToCompareWith = await apiHandler.FetchCountryByYear(httpClient, code, _date);
+            _dataMetrics = GetValidMetrics();
             StateHasChanged();
         }
 
         private static string FormatLabel(string label)
         {
             return $"{label.Replace(" ", "-")}-statistic";
+        }
+
+        private void HandleFilterChange(IEnumerable<string> selectedValues)
+        {
+            _dataMetrics = selectedValues.ToList();
+
+            // Makes sure that when you select unselected values, they end up in the same order as before
+            _dataMetrics = _dataMetrics.OrderBy(d => _availableMetrics.IndexOf(d)).ToList();
         }
     }
 }
