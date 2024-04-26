34f4408e58d6b532fc1c5f7cb9235a526ce1f309
Flaskhals68 <03kajo@gmail.com>
2024-04-26 15:35:41 +0200

    Refactor GeoLocatorHandler

--- Client/API/GeoLocatorHandler.cs
+++ Client/API/GeoLocatorHandler.cs
@@ -5,16 +5,7 @@
         private readonly string _apiUrl = "https://ipapi.co";
         public async Task<string> GetUserISOAsync(HttpClient httpClient)
         {
-            try
-            {
-                var iso = await httpClient.GetStringAsync($"{_apiUrl}/country_code_iso3/");
-                return iso;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw new Exception($"Api call failed, {e}");
-            }
+            return await new RequestWrapper<string>().GetFromJSONAsync(httpClient, $"{_apiUrl}/country_code_iso3/");
         }
     }
 }
